0f55926b4b5564d28883cab24668f0bdbc583037
JavierNavarroAbellan <jnavarroabellan@gmail.com>
2025-09-18 13:22:35 +0200

    Pruebas
    Pruebas

---
+++ UT1/src/Ejemplo1.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Ejemplo1 {
+    public static void main(String[] args) {
+        String ruta = ".";
+//        if (args.length >= 1) {
+//            ruta = args;
+//        }
+
+        File fich = new File(ruta);
+        if (!fich.exists()) {
+            System.out.println("No existe el fichero o directorio (" + ruta + ").");
+        } else {
+            if (fich.isFile()) {
+                System.out.println(ruta + " es un fichero.");
+            } else {
+                System.out.println(ruta + " es un directorio. Contenidos: ");
+                File[] ficheros = fich.listFiles(); // Ojo, ficheros o directorios 
+                for (File f : ficheros) {
+                    String textoDescr = f.isDirectory() ? "/" : "";
+                    if (f.isFile()) {
+                        textoDescr += " ";
+                    }
+                    System.out.println(textoDescr + f.getName());
+                }
+            }
+        }
+    }
+} 
